14fa857d5702b323412a24f4d43c5942428626e4
smarkoco <sean_oc@emory.edu>
2022-05-10 20:21:40 +0000

    updated functions for usability and default to 4 cameras

--- frames2vid.py
+++ frames2vid.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 
-def frames2vid(base_filename, num_frames, downsampling_factor=1, destination='.', bottom_cam_id=0, num_cams=3, framerate=100):
+def frames2vid(base_filename, num_frames, downsampling_factor=1, destination='.', num_cams=4, framerate=100): #bottom_cam_id=0
      # python command to process frames into 3 videos, flipping the bottom camera view to enable triangulation
      # COMMAND: ffmpeg -framerate 100 -f image2 -i ./Calib0_%3d_cam0.jpg -vcodec libx264 -crf 10 -pix_fmt yuv420p -filter:v "scale= iw/2:ih/2, vflip" -an Calib0_cam0.mp4
      
@@ -10,31 +10,29 @@ def frames2vid(base_filename, num_frames, downsampling_factor=1, destination='.'
           assert type(int(num_frames)) is int,"num_frames must be an integer!"
           assert type(int(downsampling_factor)) is int,"downsampling_factor must be an integer!"
      except:
-          raise Exception("Problem with num_frames input. It must be an integer!")
+          raise Exception("Problem with num_frames or downsampling_factor input. They must be integers!")
                
      
      num_frame_digits = len(str(num_frames)) # get max number of digits in frame count
      
      for ii in range(num_cams):
           iCam = "cam"+str(ii)
-          if iCam == "cam"+str(bottom_cam_id):
-               os.system(f'ffmpeg -framerate {framerate} -f image2 -i ./{base_filename}_%{num_frame_digits}d_{iCam}.jpg -vcodec libx264 -crf 10 -pix_fmt yuv420p -filter:v "scale= iw/{downsampling_factor}:ih/{downsampling_factor}, vflip" -an {destination}/{base_filename}_{iCam}.mp4')
-          else:
-               os.system(f'ffmpeg -framerate {framerate} -f image2 -i ./{base_filename}_%{num_frame_digits}d_{iCam}.jpg -vcodec libx264 -crf 10 -pix_fmt yuv420p -filter:v "scale= iw/{downsampling_factor}:ih/{downsampling_factor}" -an {destination}/{base_filename}_{iCam}.mp4')
+          # if iCam == "cam"+str(bottom_cam_id):
+          #      os.system(f'ffmpeg -framerate {framerate} -f image2 -i ./{base_filename}_%{num_frame_digits}d_{iCam}.jpg -vcodec libx264 -crf 10 -pix_fmt yuv420p -filter:v "scale= iw/{downsampling_factor}:ih/{downsampling_factor}, vflip" -an {destination}/{base_filename}_{iCam}.mp4')
+          # else:
+          os.system(f'ffmpeg -framerate {framerate} -f image2 -i ./{base_filename}_%{num_frame_digits}d_{iCam}.jpg -vcodec libx264 -crf 2 -pix_fmt gray -filter:v "scale= iw/{downsampling_factor}:ih/{downsampling_factor}" -an {destination}/{base_filename}_{iCam}.mp4')
 
 if __name__ == '__main__':
      print(list(sys.argv))
      if len(list(sys.argv)) == 3:
-          frames2vid(sys.argv[1], sys.argv[2])
+          frames2vid(base_filename=sys.argv[1], num_frames=sys.argv[2])
      elif len(list(sys.argv)) == 4:
-          frames2vid(sys.argv[1], sys.argv[2], downsampling_factor=sys.argv[3])
+          frames2vid(base_filename=sys.argv[1], num_frames=sys.argv[2], downsampling_factor=sys.argv[3])
      elif len(list(sys.argv)) == 5:
-          frames2vid(sys.argv[1], sys.argv[2], downsampling_factor=sys.argv[3], destination=sys.argv[4])
+          frames2vid(base_filename=sys.argv[1], num_frames=sys.argv[2], downsampling_factor=sys.argv[3], destination=sys.argv[4])
      elif len(list(sys.argv)) == 6:
-          frames2vid(sys.argv[1], sys.argv[2], downsampling_factor=sys.argv[3], destination=sys.argv[4], bottom_cam_id=sys.argv[5])
+          frames2vid(base_filename=sys.argv[1], num_frames=sys.argv[2], downsampling_factor=sys.argv[3], destination=sys.argv[4], num_cams=sys.argv[5])
      elif len(list(sys.argv)) == 7:
-          frames2vid(sys.argv[1], sys.argv[2], downsampling_factor=sys.argv[3], destination=sys.argv[4], bottom_cam_id=sys.argv[5], num_cams=sys.argv[6])
-     elif len(list(sys.argv)) == 7:
-          frames2vid(sys.argv[1], sys.argv[2], downsampling_factor=sys.argv[3], destination=sys.argv[4], bottom_cam_id=sys.argv[5], num_cams=sys.argv[6], framerate=sys.argv[7])
+          frames2vid(base_filename=sys.argv[1], num_frames=sys.argv[2], downsampling_factor=sys.argv[3], destination=sys.argv[4], num_cams=sys.argv[5], framerate=sys.argv[6])
      else:
-          raise Exception("enter at least 2 arguments and less than 7! :) \nInputs: base_filename, num_frames, destination, bottom_cam_id, num_cams, framerate")
+          raise Exception("enter at least 2 arguments and no more than 6! :) \nInputs: base_filename, num_frames, destination, num_cams, and framerate")
--- stackvids.py
+++ stackvids.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 
-def stackvids(base_filename, destination='.', stack_type='hstack', num_cams=3):
+def stackvids(base_filename, destination='.', stack_type='hstack', num_cams=4):
      # python command to process frames into 3 videos, flipping the bottom camera view to enable triangulation
      # COMMAND: ffmpeg -framerate 100 -f image2 -i ./Calib0_%3d_cam0.jpg -vcodec libx264 -crf 10 -pix_fmt yuv420p -filter:v "scale= iw/2:ih/2, vflip" -an Calib0_cam0.mp4
      
@@ -16,9 +16,11 @@ def stackvids(base_filename, destination='.', stack_type='hstack', num_cams=3):
      # num_frame_digits = len(str(num_frames)) # get max number of digits in frame count
      
      if num_cams==2:
-          os.system(f'ffmpeg -i {base_filename}_cam0.mp4 -i {base_filename}_cam1.mp4 -i -filter_complex {stack_type}=inputs=2 {destination}/stack_{base_filename}.mp4')
+          os.system(f'ffmpeg -i {base_filename}_cam0.mp4 -i {base_filename}_cam1.mp4 -filter_complex {stack_type}=inputs=2 {destination}/stack_{base_filename}.mp4')
      elif num_cams==3:
           os.system(f'ffmpeg -i {base_filename}_cam0.mp4 -i {base_filename}_cam1.mp4 -i {base_filename}_cam2.mp4 -filter_complex {stack_type}=inputs=3 {destination}/stack_{base_filename}.mp4')
+     elif num_cams==4:
+          os.system(f'ffmpeg -i {base_filename}_cam0.mp4 -i {base_filename}_cam1.mp4 -i {base_filename}_cam2.mp4 -i {base_filename}_cam3.mp4 -filter_complex {stack_type}=inputs=4 {destination}/stack_{base_filename}.mp4')
 
 if __name__ == '__main__':
      print(list(sys.argv))
@@ -31,4 +33,4 @@ if __name__ == '__main__':
      elif len(list(sys.argv)) == 5:
           stackvids(sys.argv[1], destination=sys.argv[2], stack_type=sys.argv[3], num_cams=sys.argv[4])
      else:
-          raise Exception("enter at least 2 arguments and less than 7! :) \nInputs: base_filename, num_frames, destination, bottom_cam_id, num_cams, framerate")
+          raise Exception("enter at least 2 arguments and no more than 4! :) \nInputs: base_filename, num_frames, destination, bottom_cam_id, num_cams, framerate")

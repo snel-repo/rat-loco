721df11e6904923624d958114afa393a2a1068dd
smarkoco <sean_oc@emory.edu>
2022-05-25 22:34:23 +0000

    replaced scp with rsync to fix bash argument overflow with large frame volume

--- getvids.py
+++ getvids.py
@@ -2,7 +2,7 @@ import sys
 import os
 import time
 
-axon_ip = "192.168.1.206"
+poisson_ip = "192.168.1.206"
 
 if __name__=='__main__':
     if len(list(sys.argv)) == 1:
@@ -10,7 +10,8 @@ if __name__=='__main__':
     elif len(list(sys.argv)) == 2:
         folder_date = sys.argv[1]
     else:
-        raise Exception("Enter no more than 1 argument for the 'FLIR_Multi_Cam_HWTrig/images' folder date. Example: 220110 for Jan. 10th 2022, will access images220110/ folder")
-    print("retrieving videos from axon.")
-    os.system("scp snel@"+axon_ip+f":/home/snel/git/FLIR_Multi_Cam_HWTrig/images{folder_date}/*.mp4 /home/snel/git/FLIR_Multi_Cam_HWTrig/images{folder_date}/")    
+        raise Exception("Enter no more than 1 argument for the 'FLIR_Multi_Cam_HWTrig/images' folder date. Example: 'getvids 220214' for Valentine's Day, 2022, will access images220214/ folder")
+    print("retrieving videos from poisson.")
+    os.system("ssh snel@"+poisson_ip+f" 'ls /home/snel/git/FLIR_Multi_Cam_HWTrig/images{folder_date}/ | grep mp4 > /home/snel/git/FLIR_Multi_Cam_HWTrig/images{folder_date}/video_list.txt'")
+    os.system("rsync -aP --rsh=ssh --files-from=snel@"+poisson_ip+f":/home/snel/git/FLIR_Multi_Cam_HWTrig/images{folder_date}/video_list.txt snel@"+poisson_ip+f":/home/snel/git/FLIR_Multi_Cam_HWTrig/images{folder_date}/ /home/snel/git/FLIR_Multi_Cam_HWTrig/images{folder_date}/")
     print("done.")

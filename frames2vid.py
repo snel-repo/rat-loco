f92200b852612e3a01724d19c23c319aa7c1b4a9
smarkoco <sean_oc@emory.edu>
2023-03-11 17:06:14 -0500

    Updated README.md to latest usage instructions

--- frames2vid.py
+++ frames2vid.py
@@ -3,8 +3,8 @@ import os
 
 
 def frames2vid(base_filename, num_frames, downsampling_factor=1, destination='.', framerate=125, num_cams=4):
-     # python command to process frames into num_cams videos, flipping the bottom camera view to enable triangulation
-     # COMMAND: ffmpeg -framerate 100 -f image2 -i ./Calib0_%3d_cam0.jpg -vcodec libx264 -crf 10 -pix_fmt yuv420p -filter:v "scale= iw/2:ih/2, vflip" -an Calib0_cam0.mp4
+     # python function to process frames into num_cams videos
+     # COMMAND: ffmpeg -framerate 125 -f image2 -i ./video0_%3d_cam0.jpg -vcodec libx264 -crf 10 -pix_fmt yuv420p -filter:v "scale= iw/2:ih/2, vflip" -an video0_cam0.mp4
      
      try:
           assert type(int(num_frames)) is int,"num_frames must be an integer!"
